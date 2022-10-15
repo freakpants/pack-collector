e4e18b0b55e9d435e401740a99ed9a2db05ac036
Steven Thewissen <github@thewissen.io>
2022-10-15 19:13:14 +0200

    Styling changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
 import { Grid, TextField } from "@mui/material";
 import Pack from "./Pack";
+import Logo from "./assets/logopc.png";
 
 class App extends Component {
   constructor(props) {
@@ -28,43 +29,43 @@ class App extends Component {
     axios
       .get(process.env.REACT_APP_AJAXSERVER + "getPacks.php")
       .then((response) => {
-        // add untradeable and tradeable = 0 for every pack
-        response.data.forEach((pack) => {
-          // look for the pack in localpacks
-          if (localPacks) {
-            const localPack = JSON.parse(localPacks).find(
-              (localPack) => localPack.id === pack.id
-            );
-            if (localPack && localPack.untradeable !== null) {
-              pack.untradeable = localPack.untradeable;
-            } else {
-              pack.untradeable = 0;
-            }
-            if (localPack && localPack.tradeable !== null) {
-              pack.tradeable = localPack.tradeable;
-            } else {
-              pack.tradeable = 0;
-            }
-          } else {
-            pack.untradeable = 0;
-            pack.tradeable = 0;
-          }
-        });
+    // add untradeable and tradeable = 0 for every pack
+    response.data.forEach((pack) => {
+      // look for the pack in localpacks
+      if (localPacks) {
+        const localPack = JSON.parse(localPacks).find(
+          (localPack) => localPack.id === pack.id
+        );
+        if (localPack && localPack.untradeable !== null) {
+          pack.untradeable = localPack.untradeable;
+        } else {
+          pack.untradeable = 0;
+        }
+        if (localPack && localPack.tradeable !== null) {
+          pack.tradeable = localPack.tradeable;
+        } else {
+          pack.tradeable = 0;
+        }
+      } else {
+        pack.untradeable = 0;
+        pack.tradeable = 0;
+      }
+    });
 
-        this.setState({ packs: response.data, allPacks: response.data }, () => {this.updateTotals();});
-        
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    this.setState({ packs: response.data, allPacks: response.data }, () => {
+      this.updateTotals();
+    });
   }
 
-  handleChange(e){
-    this.setState({packSearch: e.target.value});
+  handleChange(e) {
+    this.setState({ packSearch: e.target.value });
     // filter the pack list by packs that have the search term in their name
     // set state on packs
-    this.setState({packs: this.state.allPacks.filter((pack) => pack.name.toLowerCase().includes(e.target.value.toLowerCase()))});
-
+    this.setState({
+      packs: this.state.allPacks.filter((pack) =>
+        pack.name.toLowerCase().includes(e.target.value.toLowerCase())
+      ),
+    });
   }
 
   countUpdate = (packId, count, tradeable) => {
@@ -72,9 +73,9 @@ class App extends Component {
     // get the pack
     const pack = this.state.packs.find((pack) => pack.id === packId);
     // update the count
-    if(tradeable){
+    if (tradeable) {
       pack.tradeable = count;
-    }else{
+    } else {
       pack.untradeable = count;
     }
     // update the state
@@ -82,60 +83,49 @@ class App extends Component {
     localStorage.setItem("packs", JSON.stringify(this.state.packs));
 
     this.updateTotals();
-
   };
 
-  updateTotals(){
-        // loop all tradeable pack counts and add them together
-        let tradeablePacks = 0;
-        this.state.packs.forEach((pack) => {
-          tradeablePacks += pack.tradeable;
-        }); 
-    
-        this.setState({tradeablePacks: tradeablePacks});
-    
-        // loop all untradeable pack counts and add them together
-        let untradeablePacks = 0;
-        this.state.packs.forEach((pack) => {
-          untradeablePacks += pack.untradeable;
-        }
-        );
-    
-        this.setState({untradeablePacks: untradeablePacks});
+  updateTotals() {
+    // loop all tradeable pack counts and add them together
+    let tradeablePacks = 0;
+    this.state.packs.forEach((pack) => {
+      tradeablePacks += pack.tradeable;
+    });
 
-        // calculate total packs
-        this.setState({totalPacks: tradeablePacks + untradeablePacks});
+    this.setState({ tradeablePacks: tradeablePacks });
 
-        // calculate total coins
-        let totalCoins = 0;
-        this.state.packs.forEach((pack) => {
-          totalCoins += (pack.tradeable + pack.untradeable) * pack.coin_value;
-        }
-        );
+    // loop all untradeable pack counts and add them together
+    let untradeablePacks = 0;
+    this.state.packs.forEach((pack) => {
+      untradeablePacks += pack.untradeable;
+    });
 
-        this.setState({totalCoins: totalCoins});
+    this.setState({ untradeablePacks: untradeablePacks });
 
-        // calculate total fp
-        let totalFP = 0;
-        this.state.packs.forEach((pack) => {
-          totalFP += (pack.tradeable + pack.untradeable) * pack.fp;
-        }
-        );
+    // calculate total packs
+    this.setState({ totalPacks: tradeablePacks + untradeablePacks });
 
-        this.setState({totalFP: totalFP});
+    // calculate total coins
+    let totalCoins = 0;
+    this.state.packs.forEach((pack) => {
+      totalCoins += (pack.tradeable + pack.untradeable) * pack.coin_value;
+    });
 
+    this.setState({ totalCoins: totalCoins });
 
-        
-        // 12000 fp => 8999 EuroCents
-        let fpToEuro = 8999 / 12000;
+    // calculate total fp
+    let totalFP = 0;
+    this.state.packs.forEach((pack) => {
+      totalFP += (pack.tradeable + pack.untradeable) * pack.fp;
+    });
 
-        // calculate total cash
-        this.setState({totalCash: Math.floor((totalFP * fpToEuro))  / 100});
-        
+    this.setState({ totalFP: totalFP });
 
-        
+    // 12000 fp => 8999 EuroCents
+    let fpToEuro = 8999 / 12000;
 
-
+    // calculate total cash
+    this.setState({ totalCash: Math.floor(totalFP * fpToEuro) / 100 });
   }
 
   render() {
@@ -146,34 +136,58 @@ class App extends Component {
       },
     });
 
-
     return (
       <ThemeProvider theme={theme}>
-        <div id="top"></div>
-        <div id="bottom"></div>
-        <Grid container>
-          <Grid item xs={12}>
-            <h1>FUT 23 Pack Collector</h1>
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item xs={12} className={"statsline"}>
-            Untradeable Packs: {this.state.untradeablePacks} | Tradeable Packs: {this.state.tradeablePacks} | Total Packs: {this.state.totalPacks}  <br/>
-            Total Coins: {this.state.totalCoins} | Total FP: {this.state.totalFP} | Total Cash: {this.state.totalCash} € / {Math.floor(this.state.totalCash * 0.97 * 100)/100} $ / {Math.floor(this.state.totalCash * 0.87 * 100)/100} £
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item xs={12}>
-            <TextField value={this.state.packSearch} id="outlined-basic" label="filter by name" variant="outlined" onChange={this.handleChange} onBlur={() => this.props.actions.updateInput(this.state.inputValue)} />
-          </Grid>
-        </Grid>
-         <Grid container>
+        <div className={"logo"}>
+            <img className={"logo__img"} src={Logo} alt="FUT23 Pack Collector" />
+        </div>
+        <div className={"statistics"}>
+            <div className={"statistics__item"}>
+              <span className={"statistics__item__header"}>Untradeable</span>
+              <span className={"statistics__item__value"}>{this.state.untradeablePacks} packs</span>
+            </div>
+            <div className={"statistics__item"}>              
+              <span className={"statistics__item__header"}>Tradeable</span>
+              <span className={"statistics__item__value"}>{this.state.tradeablePacks} packs</span>
+            </div>
+            <div className={"statistics__item"}>
+              <span className={"statistics__item__header"}>Total</span>
+              <span className={"statistics__item__value"}>{this.state.totalPacks} packs</span>
+            </div>
+            <div className={"statistics__item"}>
+              <span className={"statistics__item__header"}>Total Coins</span>
+              <span className={"statistics__item__value statistics__item__coins"}>{this.state.totalCoins}</span>
+            </div>
+            <div className={"statistics__item"}>
+              <span className={"statistics__item__header"}>Total FP</span>
+              <span className={"statistics__item__value statistics__item__points"}>{this.state.totalFP}</span>
+            </div>
+            <div className={"statistics__item"}>
+              <span className={"statistics__item__header"}>Total Cash</span>
+              <span className={"statistics__item__value"}>€{this.state.totalCash} /{" "}
+              ${Math.floor(this.state.totalCash * 0.97 * 100) / 100} /{" "}
+              £{Math.floor(this.state.totalCash * 0.87 * 100) / 100}
+              </span>
+            </div>
+        </div>
+        <div className="filter">
+            <input
+              value={this.state.packSearch}
+              id="outlined-basic"
+              label="filter by name"
+              variant="outlined"
+              placeholder="Search for a specific pack..."
+              onChange={this.handleChange}
+              onBlur={() =>
+                this.props.actions.updateInput(this.state.inputValue)
+              }
+            />
+        </div>
+        <div className={"packs"}>
           {this.state.packs.map((pack) => (
-            <Grid key={pack.id} item xs={2}>
-              <Pack onCountUpdate={this.countUpdate} pack={pack}/>
-            </Grid>
+              <Pack key={pack.id} onCountUpdate={this.countUpdate} pack={pack} />
           ))}
-        </Grid>
+        </div>
       </ThemeProvider>
     );
   }
