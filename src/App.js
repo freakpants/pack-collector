a02d70d55c881b267f19201147b1a82051c5fe0c
freakpants <freakpants@gmail.com>
2022-10-18 18:11:20 +0200

    spreadsheet mode

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
 import Pack from "./Pack";
 import Logo from "./assets/logopc.png";
+import { FormControlLabel, FormGroup, Checkbox } from "@mui/material";
 
 class App extends Component {
   constructor(props) {
@@ -17,9 +18,11 @@ class App extends Component {
       totalPlayers: 0,
       totalFP: 0,
       totalCash: 0,
+      spreadSheetMode: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.updateTotals = this.updateTotals.bind(this);
+    this.handleSpreadMode = this.handleSpreadMode.bind(this);
   }
 
   componentDidMount() {
@@ -28,33 +31,33 @@ class App extends Component {
     axios
       .get(process.env.REACT_APP_AJAXSERVER + "getPacks.php")
       .then((response) => {
-    // add untradeable and tradeable = 0 for every pack
-    response.data.forEach((pack) => {
-      // look for the pack in localpacks
-      if (localPacks) {
-        const localPack = JSON.parse(localPacks).find(
-          (localPack) => localPack.id === pack.id
-        );
-        if (localPack && localPack.untradeable !== null) {
-          pack.untradeable = localPack.untradeable;
-        } else {
-          pack.untradeable = 0;
-        }
-        if (localPack && localPack.tradeable !== null) {
-          pack.tradeable = localPack.tradeable;
-        } else {
-          pack.tradeable = 0;
-        }
-      } else {
-        pack.untradeable = 0;
-        pack.tradeable = 0;
-      }
-    });
+        // add untradeable and tradeable = 0 for every pack
+        response.data.forEach((pack) => {
+          // look for the pack in localpacks
+          if (localPacks) {
+            const localPack = JSON.parse(localPacks).find(
+              (localPack) => localPack.id === pack.id
+            );
+            if (localPack && localPack.untradeable !== null) {
+              pack.untradeable = localPack.untradeable;
+            } else {
+              pack.untradeable = 0;
+            }
+            if (localPack && localPack.tradeable !== null) {
+              pack.tradeable = localPack.tradeable;
+            } else {
+              pack.tradeable = 0;
+            }
+          } else {
+            pack.untradeable = 0;
+            pack.tradeable = 0;
+          }
+        });
 
-    this.setState({ packs: response.data, allPacks: response.data }, () => {
-      this.updateTotals();
-    });
-  });
+        this.setState({ packs: response.data, allPacks: response.data }, () => {
+          this.updateTotals();
+        });
+      });
   }
 
   handleChange(e) {
@@ -66,6 +69,10 @@ class App extends Component {
         pack.name.toLowerCase().includes(e.target.value.toLowerCase())
       ),
     });
+  }
+
+  handleSpreadMode(e) {
+    this.setState({ spreadSheetMode: e.target.checked });
   }
 
   countUpdate = (packId, count, tradeable) => {
@@ -133,62 +140,125 @@ class App extends Component {
       typography: {
         fontFamily: "Matroska",
         fontSize: 12,
+        color: "#F8EEDE",
       },
     });
 
     return (
       <ThemeProvider theme={theme}>
         <div className={"logo"}>
-            <img className={"logo__img"} src={Logo} alt="FUT23 Pack Collector" />
+          <img className={"logo__img"} src={Logo} alt="FUT23 Pack Collector" />
         </div>
         <div className={"statistics"}>
-            <div className={"statistics__item"}>
-              <span className={"statistics__item__header"}>Untradeable</span>
-              <span className={"statistics__item__value"}>{this.state.untradeablePacks} packs</span>
-            </div>
-            <div className={"statistics__item"}>              
-              <span className={"statistics__item__header"}>Tradeable</span>
-              <span className={"statistics__item__value"}>{this.state.tradeablePacks} packs</span>
-            </div>
-            <div className={"statistics__item"}>
-              <span className={"statistics__item__header"}>Total</span>
-              <span className={"statistics__item__value"}>{this.state.totalPacks} packs</span>
-            </div>
-            <div className={"statistics__item"}>
-              <span className={"statistics__item__header"}>Total Coins</span>
-              <span className={"statistics__item__value statistics__item__coins"}>{this.state.totalCoins}</span>
-            </div>
-            <div className={"statistics__item"}>
-              <span className={"statistics__item__header"}>Total FP</span>
-              <span className={"statistics__item__value statistics__item__points"}>{this.state.totalFP}</span>
-            </div>
-            <div className={"statistics__item"}>
-              <span className={"statistics__item__header"}>Total Cash</span>
-              <span className={"statistics__item__value"}>€{this.state.totalCash} /{" "}
-              ${Math.floor(this.state.totalCash * 0.97 * 100) / 100} /{" "}
-              £{Math.floor(this.state.totalCash * 0.87 * 100) / 100}
-              </span>
-            </div>
+          <div className={"statistics__item"}>
+            <span className={"statistics__item__header"}>Untradeable</span>
+            <span className={"statistics__item__value"}>
+              {this.state.untradeablePacks} packs
+            </span>
+          </div>
+          <div className={"statistics__item"}>
+            <span className={"statistics__item__header"}>Tradeable</span>
+            <span className={"statistics__item__value"}>
+              {this.state.tradeablePacks} packs
+            </span>
+          </div>
+          <div className={"statistics__item"}>
+            <span className={"statistics__item__header"}>Total</span>
+            <span className={"statistics__item__value"}>
+              {this.state.totalPacks} packs
+            </span>
+          </div>
+          <div className={"statistics__item"}>
+            <span className={"statistics__item__header"}>Total Coins</span>
+            <span className={"statistics__item__value statistics__item__coins"}>
+              {this.state.totalCoins}
+            </span>
+          </div>
+          <div className={"statistics__item"}>
+            <span className={"statistics__item__header"}>Total FP</span>
+            <span
+              className={"statistics__item__value statistics__item__points"}
+            >
+              {this.state.totalFP}
+            </span>
+          </div>
+          <div className={"statistics__item"}>
+            <span className={"statistics__item__header"}>Total Cash</span>
+            <span className={"statistics__item__value"}>
+              €{this.state.totalCash} / $
+              {Math.floor(this.state.totalCash * 0.97 * 100) / 100} / £
+              {Math.floor(this.state.totalCash * 0.87 * 100) / 100}
+            </span>
+          </div>
         </div>
         <div className="filter">
-            <input
-              value={this.state.packSearch}
-              id="outlined-basic"
-              label="filter by name"
-              variant="outlined"
-              placeholder="Search for a specific pack..."
-              onChange={this.handleChange}
-              onBlur={() =>
-                this.props.actions.updateInput(this.state.inputValue)
-              }
+
+          <FormGroup>
+          <input
+            value={this.state.packSearch}
+            id="outlined-basic"
+            label="filter by name"
+            variant="outlined"
+            placeholder="Search for a specific pack..."
+            onChange={this.handleChange}
+            onBlur={() => this.props.actions.updateInput(this.state.inputValue)}
+          />
+            <FormControlLabel
+              control={<Checkbox onChange={this.handleSpreadMode} />}
+              label="Spreadsheet Mode"
             />
+          </FormGroup>
         </div>
+        { ! this.state.spreadSheetMode  && (
         <div className={"packs"}>
           {this.state.packs.map((pack) => (
-              <Pack key={pack.id} onCountUpdate={this.countUpdate} pack={pack} />
+            <Pack key={pack.id} onCountUpdate={this.countUpdate} pack={pack} />            
           ))}
         </div>
-      </ThemeProvider>
+        )}
+
+        { this.state.spreadSheetMode  && (
+        <div className={"spreadsheet"}>
+          <table className={"packs-table"}>
+            <thead>
+              <tr>
+                <th>Pack</th>
+                <th>Tradeable</th>
+                <th>Untradeable</th>
+                <th>Total</th>
+                <th>Coins</th>
+                <th>FP</th>
+                <th>Cash</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.packs.map((pack) => (    
+                <tr>
+                  <td>{pack.name}</td>
+                  <td>{pack.tradeable}</td>
+                  <td>{pack.untradeable}</td>
+                  <td>{pack.tradeable + pack.untradeable}</td>
+                  <td>{pack.coin_value * (pack.tradeable + pack.untradeable)}</td>
+                  <td>{pack.fp * (pack.tradeable + pack.untradeable)}</td>
+                  <td>{Math.floor((pack.fp * (pack.tradeable + pack.untradeable)) * 8999 / 12000) / 100}</td>
+
+                </tr>
+              ))}
+              <tr>
+                <td>Total</td>
+                <td>{this.state.tradeablePacks}</td>
+                <td>{this.state.untradeablePacks}</td>
+                <td>{this.state.totalPacks}</td>
+                <td>{this.state.totalCoins}</td>
+                <td>{this.state.totalFP}</td>
+                <td>{this.state.totalCash}</td>
+
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        )}
+        </ThemeProvider>
     );
   }
 }
