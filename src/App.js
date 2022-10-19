785cd9afe0c3512e8bce45f47434d97c41b1690e
freakpants <freakpants@gmail.com>
2022-10-19 12:24:20 +0200

    absolute positioning of login area

--- src/App.js
+++ src/App.js
@@ -88,30 +88,6 @@ class App extends Component {
       }
     });
 
-    /* getRedirectResult(this.auth)
-      .then((result) => {
-        // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-        // You can use these server side with your app's credentials to access the Twitter API.
-        const credential = TwitterAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        const secret = credential.secret;
-        // ...
-
-        // The signed-in user info.
-        const user = result.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = TwitterAuthProvider.credentialFromError(error);
-        // ...
-      }); */
   }
 
   addDefaultCounts(packs) {
@@ -350,7 +326,7 @@ class App extends Component {
           />
         )}
         {this.state.user && (
-          <div className={"cloud-area"}>
+          <div className={"cloudArea"}>
             <div className={"displayName"}>
               Logged in as {this.state.user.displayName}
             </div>
